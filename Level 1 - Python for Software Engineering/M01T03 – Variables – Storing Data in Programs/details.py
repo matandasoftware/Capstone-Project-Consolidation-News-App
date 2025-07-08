5c48423201bfd84845d8d72625ef06d7cecd5bf7
matandasoftware <pfarelochannel@gmail.com>
2025-07-08 20:51:57 +0200

    Update details.py

--- Level 1 - Python for Software Engineering/M01T03 – Variables – Storing Data in Programs/details.py
+++ Level 1 - Python for Software Engineering/M01T03 – Variables – Storing Data in Programs/details.py
@@ -1,5 +1,5 @@
-name = input("Please enter your name: ")
-age = input("please enter your age: ")
-housenum = input("please enter your house number: ")
-streetname =input("please enter your street name: ")
-print(f"This is {name}. He is {age}  years old and lives at {housenum} {streetname} street")
+name = input("Please enter your name: ")  # ask user to enter name 
+age = input("please enter your age: ")     # ask user to enter their age
+housenum = input("please enter your house number: ")   # ask user to enter their house number 
+streetname =input("please enter your street name: ")   # ask user to enter their street name 
+print(f"This is {name}. He is {age}  years old and lives at {housenum} {streetname} street") # print all of entered user details in one sentence in a format string 
