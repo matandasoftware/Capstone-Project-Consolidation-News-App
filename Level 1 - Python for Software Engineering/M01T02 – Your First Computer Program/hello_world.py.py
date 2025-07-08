64231aee852e8cc7ab4a19aa356e449ff02d11b5
matandasoftware <pfarelochannel@gmail.com>
2025-07-08 06:34:26 +0200

    Add files via upload
    complete first program task

---
+++ Level 1 - Python for Software Engineering/M01T02 – Your First Computer Program/hello_world.py.py
@@ -0,0 +1,9 @@
+# get name of user via an input function then ptint it out
+# get user's age via an input function then print it out
+# print the string "Hello, World"
+
+name = input("enter your name: ")
+print(f"{name}")
+age = input("enter your age: ")
+print(f"{age}")
+print("Hello, World!")
